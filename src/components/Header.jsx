581a4a5a5fe0bdd12392619084757f7815b81f4b
Rmoreno3 <reynaldomoreno173@gmail.com>
2021-09-13 10:21:28 -0400

    modifico navBar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,37 +1,39 @@
-import React from 'react';
-import logo from '../assets/static/images/logo.png';
+/* eslint-disable quotes */
+/* eslint-disable jsx-quotes */
+import React from "react";
+import logo from "../assets/static/images/logo.png";
 
 const Header = () => (
-  <header className='header'>
-    <div className='wrapper'>
-      <div className='header__content'>
-        <figure className='header__logo_container'>
-          <img src={logo} alt='logo' />
+  <header className="header">
+    <div className="wrapper">
+      <div className="header__content">
+        <figure className="header__logo_container">
+          <img src={logo} alt="logo" />
         </figure>
-        <nav className='header__nav'>
+        <nav className="header__nav">
           <ul>
-            <li className='link'>
-              <a aria-label='Hola' href='#hero'>
+            <li className="link">
+              <a aria-label="Hola" href="#hero">
                 Hola
               </a>
             </li>
-            <li className='link'>
-              <a href='#proyectos' aria-label='Proyectos'>
+            <li className="link">
+              <a href="#proyectos" aria-label="Proyectos">
                 Proyectos
               </a>
             </li>
-            <li className='link'>
-              <a href='#contacto' aria-label='Hablemos'>
+            {/* <li className="link">
+              <a href="#contacto" aria-label="Hablemos">
                 Hablemos
               </a>
-            </li>
+            </li> */}
           </ul>
         </nav>
-        <a href='https://docs.google.com/document/d/1-Z0kdvRDF4WmYlnBiX_VLSrKsO16z6nUKhu7em5e38Q/edit?usp=sharing' target='_blank' rel='noreferrer'>
+        {/* <a href='https://docs.google.com/document/d/1-Z0kdvRDF4WmYlnBiX_VLSrKsO16z6nUKhu7em5e38Q/edit?usp=sharing' target='_blank' rel='noreferrer'>
           <button className='btn-primary' aria-label='Curriculum' type='button'>
             Curriculum
           </button>
-        </a>
+        </a> */}
       </div>
     </div>
   </header>
